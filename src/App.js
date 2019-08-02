48cd26df69761777f6eb88ea8c76d0812c518cab
Miguel López <miguellopez@vitalware.com>
2019-08-02 12:29:06 -0500

    import all components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import { Header, Form, EventsList } from './components';
+import CategoriesProvider from './context/CategoriesContext';
+import EventsProvider from './context/EventsContext';
 
 function App() {
-  return <h1>Hi</h1>;
+  return (
+    <EventsProvider>
+      <CategoriesProvider>
+        <Header />
+        <div className="uk-container">
+          <Form />
+          <EventsList />
+        </div>
+      </CategoriesProvider>
+    </EventsProvider>
+  );
 }
 
 export default App;
